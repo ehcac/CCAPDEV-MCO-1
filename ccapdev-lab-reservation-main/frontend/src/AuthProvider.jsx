889c1339b48962edad062ff138de22fc219a37e3
ehcac <cabatay.chachi@gmail.com>
2025-04-02 17:09:56 +0800

    .

--- ccapdev-lab-reservation-main/frontend/src/AuthProvider.jsx
+++ ccapdev-lab-reservation-main/frontend/src/AuthProvider.jsx
@@ -14,7 +14,7 @@ const AuthProvider = ({ children }) => {
 
     const loginAction = async (username, password, remember) => {
         try {
-            const response = await fetch(`${apiUrl}/api/auth/login`, {
+            const response = await fetch(`${process.env.REACT_APP_API_URL.replace(/\/$/, "")}/api/auth/login`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email: username, password }) 
